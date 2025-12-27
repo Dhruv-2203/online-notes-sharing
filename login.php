0b20891a67db22cccc2f02ff578989e6f61bbeb9
Dhruv <acharyadhruv206@gmail.com>
2025-12-27 10:04:05 +0530

    Added login system

---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+include "db.php";
+session_start();
+
+if (isset($_POST['login'])) {
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_name'] = $user['name'];
+            echo "Login successful!";
+        } else {
+            echo "Wrong password!";
+        }
+    } else {
+        echo "User not found!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form method="POST">
+    <input type="email" name="email" placeholder="Enter Email" required><br><br>
+    <input type="password" name="password" placeholder="Enter Password" required><br><br>
+    <button type="submit" name="login">Login</button>
+</form>
+
+</body>
+</html>
